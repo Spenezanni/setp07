fa50cc4902716c46bcd359f242495933a5666df9
Spenezanni <gustavospenezanni@gmail.com>
2021-09-13 19:57:41 -0300

    Commit Init

---
+++ src/main/java/br/com/sept07/Sept07Application.java
@@ -0,0 +1,13 @@
+package br.com.sept07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sept07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sept07Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sept07/Sept07ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sept07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sept07ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
