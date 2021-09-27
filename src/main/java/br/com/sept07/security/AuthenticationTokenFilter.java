238a38012fad25b251aa7b245b0fd68088bd2e75
Spenezanni <gustavospenezanni@gmail.com>
2021-09-27 12:19:20 -0300

    Implementing Recovering the Token

--- src/main/java/br/com/sept07/security/AuthenticationTokenFilter.java
+++ src/main/java/br/com/sept07/security/AuthenticationTokenFilter.java
@@ -1,5 +1,32 @@
 package br.com.sept07.security;
 
-public class AuthenticationTokenFilter {
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class AuthenticationTokenFilter extends OncePerRequestFilter {
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {		
+		String token = recuperarToken(request);
+        System.out.println(token);
+		filterChain.doFilter(request, response);
+			
+	}
+
+	private String recuperarToken(HttpServletRequest request) {
+		//faz um get do token no header
+		String tokenHeader = request.getHeader("Authorization");
+		if(tokenHeader == null || tokenHeader.isEmpty() || !tokenHeader.startsWith("Bearer ")) {
+			return null;			
+		}
+		return tokenHeader.substring(7, tokenHeader.length());
+	}
 
 }
